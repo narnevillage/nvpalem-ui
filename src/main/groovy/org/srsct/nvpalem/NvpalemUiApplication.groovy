c7fe3ee91b84231b95b63adefc187032b5e7050c
Srinivasulu Chappidi <srini73edu@gmail.com>
2018-02-04 18:48:26 -0500

    Added web project the UI

---
+++ src/main/groovy/org/srsct/nvpalem/NvpalemUiApplication.groovy
@@ -0,0 +1,12 @@
+package org.srsct.nvpalem
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NvpalemUiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run NvpalemUiApplication, args
+	}
+}
